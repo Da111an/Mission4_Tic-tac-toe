63a37fd962c9097d26a824ba0026a581ed77b92b
Da111an <dallangrow@gmail.com>
2026-01-28 14:06:02 -0700

    starting on the game flow

--- Program.cs
+++ Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        SupportingClass sp = new SupportingClass();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double playerTurn = 1;
+            
+            
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+            Console.WriteLine("Board initialized: ");
+
+
+            string[] board = ["1","2","3","4","5","6","7","8","9"];
+
+            // print the tic-tac-toe board with numbers in their respective places
+            if (playerTurn == 1)
+            {
+                Console.WriteLine("Player 1, please choose the number that corresponds to the locaiton you want to select.");
+
+            }
+
+
+
         }
     }
 }
