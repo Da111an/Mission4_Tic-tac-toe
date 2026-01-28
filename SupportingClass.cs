7703035313ee5cdc1c61f91304f4e84eeabf5d8a
porterraz <153124521+porterraz@users.noreply.github.com>
2026-01-28 14:24:00 -0700

    PrintBoard method

--- SupportingClass.cs
+++ SupportingClass.cs
@@ -6,5 +6,13 @@ namespace Mission4
 {
     internal class SupportingClass
     {
+        public void PrintBoard(string[] board)
+        {
+            Console.WriteLine($"{board[0]} | {board[1]} | {board[2]}");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($"{board[3]} | {board[4]} | {board[5]}");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($"{board[6]} | {board[7]} | {board[8]}");
+        }
     }
 }
